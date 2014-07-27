323859ff6d9772ca75b01bc6831480ce2330a7a5
akshay kumar gupta <g.akshay2007@gmail.com>
2014-07-27 21:44:11 +0530

    Demo Spring Program

---
+++ Copy of DemoSpring/WebContent/index.jsp
@@ -0,0 +1,1 @@
+<jsp:forward page="/showHome"></jsp:forward>
---
+++ Copy of DemoSpring/src/com/akshay/beans/UserBean.java
@@ -0,0 +1,131 @@
+package com.akshay.beans;
+
+/**
+ * This is the bean class, used for all User related operations.
+ * 
+ * @author AkshayKrGupta
+ * @version 1.0
+ */
+public class UserBean {
+	private int id;
+	private String name;
+	private String city;
+	private int age;
+	private String country;
+	private String language;
+	private String contact;
+	private String description;
+
+	/**
+	 * @return id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return country
+	 */
+	public String getCountry() {
+		return country;
+	}
+
+	/**
+	 * @param country
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	/**
+	 * @return language
+	 */
+	public String getLanguage() {
+		return language;
+	}
+
+	/**
+	 * @param language
+	 */
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	/**
+	 * @return contact
+	 */
+	public String getContact() {
+		return contact;
+	}
+
+	/**
+	 * @param contact
+	 */
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	/**
+	 * @return description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
